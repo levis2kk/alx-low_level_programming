6835ec14fbdeb1885da0d329d55aa34469b624ea
Levi Nwachukwu <levis2kk@gmail.com>
2023-07-14 15:43:44 -0700

    0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -9,25 +9,24 @@
  */
 int main(void)
 {
-    int n;
+	int n;
+	
+	srand(time(0));
+       	n = rand() - RAND_MAX / 2;
 
-    srand(time(0));
-    n = rand() - RAND_MAX / 2;
+	printf("%d", n);
 
-    printf("The number %d", n);
-
-    if (n > 0)
-    {
-        printf(" is positive.\n");
-    }
-    else if (n == 0)
-    {
-        printf(" is zero.\n");
-    }
-    else
-    {
-        printf(" is negative.\n");
-    }
-
-    return (0);
+	if (n > 0)
+	{
+		printf(" is positive.\n");
+	}
+	else if (n == 0)
+	{
+		printf(" is zero.\n");
+	}
+	else
+	{
+		printf(" is negative.\n");
+	}
+	return (0);
 }

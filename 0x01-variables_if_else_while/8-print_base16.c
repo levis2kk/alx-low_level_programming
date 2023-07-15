338d095672a80e99f3eb91aee4a32151fa8ebc7d
Levi Nwachukwu <levis2kk@gmail.com>
2023-07-14 18:21:47 -0700

    8

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,16 +1,21 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-    char hexDigits[] = "0123456789abcdef";
-    int i;
+	char hexDigits[] = "0123456789abcdef";
+	int i;
 
-    for (i = 0; i < 16; i++)
-    {
-        putchar(hexDigits[i]);
-    }
+	for (i = 0; i < 16; i++)
+	{
+		putchar(hexDigits[i]);
+	}
 
-    putchar('\n');
+	putchar('\n');
 
-    return 0;
+	return (0);
 }

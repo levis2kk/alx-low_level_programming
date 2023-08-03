d7231d6d90537636b326f261bba80496e8bc0e3d
Levi Nwachukwu <levis2kk@gmail.com>
2023-08-03 15:24:13 -0700

    update' git push

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -22,3 +22,27 @@ return (-1);
 }
 return (0 + power_operation(n, (i + 1)));
 }
+
+/**
+ * _sqrt_recursion - function that returns the natural
+ * square root of a number.
+ * @n: integer variable of given number
+ * Return: the square root of number
+ */
+
+int _sqrt_recursion(int n)
+{
+if (n < 0)
+{
+return (-1);
+}
+else if (n == 0)
+{
+return (0);
+}
+else if (n == 1)
+{
+return (1);
+}
+return (power_operation(n, 2));
+}
--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -22,3 +22,27 @@ int is_prime(int n, int i)
 		return (is_prime(n, i + 1));
 	}
 }
+
+/**
+ * is_prime_number - function that returns 1,
+ * if the input integer is a prime number.
+ * @n: natural number
+ *
+ * Return: 1 if is prime otherwise 0.
+ */
+
+int is_prime_number(int n)
+{
+	if (n <= 1)
+	{
+		return (0);
+	}
+	else if (n == 2)
+	{
+		return (1);
+	}
+	else
+	{
+		return (is_prime(n, 2));
+	}
+}

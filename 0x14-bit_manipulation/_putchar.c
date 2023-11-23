8c8791e73cabac777ad4489fae91944434514ee4
Levi <levis2kk@gmail.com>
2023-11-23 13:01:20 -0800

    Task 1

---
+++ 0x14-bit_manipulation/_putchar.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+
+/**
+ * _putchar - Writes a character to standard output (stdout)
+ * @c: The character to write
+ *
+ * Return: On success 1, on error -1.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
